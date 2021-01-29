4ab5100140f904f9a70adc8ae9fef76c6a4ef97e
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-01-29 10:49:29 +0700

    learn tutorial object(revisited)

---
+++ 1.objectrevisited/Literal/script.js
@@ -0,0 +1,51 @@
+// buat element baru
+const pBaru = document.createElement('p');
+const teksPBaru = document.createTextNode('Object Litelar');
+
+// simpan tulisan ke dalam paragraf 
+pBaru.appendChild(teksPBaru);
+
+// simpan pBaru di akhir section A
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+const button1 = document.createElement('button');
+const textbutton1 = document.createTextNode('makan');
+
+// simpan tulisan kedalam button
+button1.appendChild(textbutton1);
+sectionA.appendChild(button1);
+
+
+
+// 1. object Literal
+let mahasiswa1 = {
+    nama: 'Gunawan',
+    energi: 10,
+    makan: function(porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Halo ${this.nama} Selamat Makan`);
+        alert(`Halo ${this.nama} Selamat Makan`);
+
+    }
+}
+let mahasisw2 = {
+    nama: 'Gunawan',
+    energi: 10,
+    makan: function(porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Halo ${this.nama} Selamat Makan`);
+        alert(`Halo ${this.nama} Selamat Makan`);
+
+    }
+}
+
+button1.addEventListener('click', function() {
+    const inputporsi = prompt("Jumlah Porsi Makan");
+    const jmlporsi = parseInt(inputporsi);
+    mahasiswa1.makan(jmlporsi);
+    for (const mhs in mahasiswa1) {
+        console.log(`${mhs}: ${mahasiswa1[mhs]}`);
+        alert(`${mhs}: ${mahasiswa1[mhs]}`);
+    }
+});
