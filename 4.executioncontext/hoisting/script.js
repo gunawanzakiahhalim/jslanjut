de33cd12e16b8eb7731617e4995ecb367faa4e7c
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-01-30 09:28:17 +0700

    create new folder execution context

---
+++ 4.executioncontext/hoisting/script.js
@@ -0,0 +1,15 @@
+// Buat Element Judul
+const judul = document.createElement('h1');
+const text = document.createTextNode('Hoisting');
+judul.appendChild(text);
+document.body.appendChild(judul);
+
+// 2.1 EXECUTION CONTEXT, HOISTING & SCOPE
+
+console.log(nama);
+var nama = 'Sandika'
+
+// creation phase pada Global Context
+// nama var = undefined
+// nama function = fn()
+// hoisting
