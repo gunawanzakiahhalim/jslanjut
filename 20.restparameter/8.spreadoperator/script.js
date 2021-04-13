9512f9ba3b4d24ab6282ea30233c50e94c8e4879
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-04-13 16:50:59 +0700

    rest parameter spreadoperator

---
+++ 20.restparameter/8.spreadoperator/script.js
@@ -0,0 +1,24 @@
+const title = document.createElement('title');
+const titletxt = document.createTextNode('Rest Parameter');
+title.appendChild(titletxt);
+document.head.appendChild(title);
+
+const judul = document.createElement('h1');
+const txt = document.createTextNode('Spread Operator');
+judul.appendChild(txt);
+document.body.appendChild(judul);
+
+// Rest Parameter
+
+function myFunc() {
+    // return Array.from(arguments);
+    return [...arguments];
+}
+
+console.log(myFunc(1,2,3,4,5));
+const cetak = myFunc(1,2,3,4,5);
+
+
+const section = document.createElement('section');
+section.innerHTML = cetak;
+document.body.appendChild(section);
