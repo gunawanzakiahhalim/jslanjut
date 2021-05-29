1633cb38000a62afe77abc248f2a4d7cdf023565
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-05-29 17:46:47 +0700

    filtering number string and boolean

---
+++ 20.restparameter/14.string/script.js
@@ -0,0 +1,24 @@
+const title = document.createElement('title');
+const titletxt = document.createTextNode('Rest Parameter');
+title.appendChild(titletxt);
+document.head.appendChild(title);
+
+const judul = document.createElement('h1');
+const txt = document.createTextNode('Filtering String');
+judul.appendChild(txt);
+document.body.appendChild(judul);
+
+// Filtering
+
+function filterBy(type, ...values) {
+  return values.filter((v) => typeof v === type);
+}
+
+const filter = filterBy('string', 1, 2, 'Gunawan', false, 10, true, 'Zakiah');
+
+console.log(filter);
+const cetak = filter;
+
+const section = document.createElement('section');
+section.innerHTML = cetak;
+document.body.appendChild(section);
