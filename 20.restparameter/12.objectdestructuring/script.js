cf76fba80f8330fd98d8c13ab2f9a07e9d21b279
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-05-29 09:40:24 +0700

    object destructuring

---
+++ 20.restparameter/12.objectdestructuring/script.js
@@ -0,0 +1,26 @@
+const title = document.createElement('title');
+const titletxt = document.createTextNode('Rest Parameter');
+title.appendChild(titletxt);
+document.head.appendChild(title);
+
+const judul = document.createElement('h1');
+const txt = document.createTextNode('Object Destructuring');
+judul.appendChild(txt);
+document.body.appendChild(judul);
+
+// Object Destructuring
+
+const team = {
+  pm: 'Gunawan',
+  frontEnd1: 'Zakiah',
+  frontEnd2: 'Halim',
+  backEnd: 'Rikaz',
+  ux: 'Ilya',
+  devOps: 'Rara',
+};
+console.log(team);
+const cetak = team;
+
+const section = document.createElement('section');
+section.innerHTML = cetak;
+document.body.appendChild(section);
