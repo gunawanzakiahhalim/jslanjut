79d2ea24713c0f367a17a24e267cdd1d504e7807
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-06-14 13:15:04 +0700

    create tutorial asyncrhronous javascript

---
+++ 21.asynchronous/1.boolean/script.js
@@ -0,0 +1,24 @@
+const title = document.createElement('title');
+const titletxt = document.createTextNode('Asynchronous Javascript');
+title.appendChild(titletxt);
+document.head.appendChild(title);
+
+const judul = document.createElement('h1');
+const txt = document.createTextNode('Filtering Boolean');
+judul.appendChild(txt);
+document.body.appendChild(judul);
+
+// Filtering
+
+function filterBy(type, ...values) {
+  return values.filter((v) => typeof v === type);
+}
+
+const filter = filterBy('boolean', 1, 2, 'Gunawan', false, 10, true, 'Zakiah');
+
+console.log(filter);
+const cetak = filter;
+
+const section = document.createElement('section');
+section.innerHTML = cetak;
+document.body.appendChild(section);
